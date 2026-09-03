95ba959fe810671352de7969d44524d834b550f8
Brian <Byoo0326@gmail.com>
2026-09-03 12:07:38 -0700

    Cleaner Code and Additional Functionality: - Combined the event selectors and put them into the CreateGrid function instead of having them outside - Made a button and resolution indicator above the sketchboard - Various fixes

--- Sketch.js
+++ Sketch.js
@@ -1,7 +1,7 @@
 //Etch a Sketch!
 
 
-//first, lets create the function that will create the grid
+//first, lets create the function that will create the grid and its coloring logic
 const container = document.querySelector(".container");
 function createGrid(length) {
     for (i = 0; i < length; i++) {
@@ -10,14 +10,21 @@ function createGrid(length) {
         container.appendChild(column);
         for (j = 0; j < length; j++) {
             let row = column.appendChild(document.createElement("div"));
+            row.addEventListener("mouseenter", () => {
+                row.setAttribute("style", "background-color : black");
+            });
         }
     }
 }
 
 //lets call on that function with a width_height of 16
 let width_height = 164;
+let numOfDivs = width_height * width_height;
 createGrid(width_height);
 const squares = container.querySelectorAll(".columns div");
+const resolutionIndicator = container.querySelector("p");
+const resolutionButton = document.querySelector(".button_UI").querySelector(".resolutionSelect");
+resolutionButton.addEventListener("click", resolutionSelector)
 
 //lets set up the event listeners
 //we will make it so when the mouse enters a square, it is black, and when it comes off, it turns transparent
@@ -30,7 +37,23 @@ squares.forEach((square) => {
     });
 });
 
+//instead of manually changing width_height in the code, we can make a function that listens for a button call
+//the function will then take user input to determine the screen's resolution
+//lets have this function choose a new width_height to call createGrid() with, then clear the board
+function resolutionSelector() {
+    let resolutionSelector = Number(prompt("Select your desired resolution:"));
+    alert(`Sketchboard resolution is now ${resolutionSelector} x ${resolutionSelector}!`);
+    resolutionIndicator.textContent = `${resolutionSelector} x ${resolutionSelector}`;
+    for (i = 0; i < numOfDivs; i++) {
+        container.removeChildAll("div");
+    }
+    createGrid(resolutionSelector);
+}
 
+
+// row.addEventListener("mouseout", () => {
+//                 row.setAttribute("style", "background-color : transparent");
+//             });
 
 
 
